68dcdfc56546a72a4c725d9e903e381d368446bf
Shahram Ghassemi <shahramg92@hotmail.com>
2018-09-26 23:19:33 -0500

    update

--- nativeplaces/App.js
+++ nativeplaces/App.js
@@ -3,11 +3,13 @@ import { StyleSheet, View } from 'react-native';
 
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import PlaceList from './src/components/PlaceList/PlaceList';
+import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
 
 type Props = {};
 export default class App extends Component<Props> {
   state = {
-    places: []
+    places: [],
+    selectedPlace: null
   };
 
   placeAddedHandler = placeName => {
@@ -24,11 +26,28 @@ export default class App extends Component<Props> {
     })
   }
 
-  placeDeletedHandler = key => {
+  placeDeletedHandler = () => {
     this.setState(prevState => {
       return {
         places: prevState.places.filter(place => {
-          return place.key !== key;
+          return place.key !== prevState.selectedPlace.key;
+        }),
+        selectedPlace: null
+      };
+    });
+  }
+
+  modalClosedHandler = () => {
+    this.setState({
+      selectedPlace: null
+    })
+  }
+
+  placeSeletedHandler = key => {
+    this.setState(prevState => {
+      return {
+        selectedPlace: prevState.places.find(place => {
+          return place.key === key;
         })
       };
     });
@@ -37,10 +56,13 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+        <PlaceDetail    selectedPlace={this.state.selectedPlace}
+        onItemDeleted={this.placeDeletedHandler}
+        onModalClosed={this.modalClosedHandler}/>
         <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
         <PlaceList
           places={this.state.places}
-          onItemDeleted={this.placeDeletedHandler}/>
+          onItemSelected={this.placeSeletedHandler}/>
       </View>
     );
   }
