bfc58903a513f7c4f65b71155e2b73d78d656f95
Shahram Ghassemi <shahramg92@hotmail.com>
2018-09-26 21:45:30 -0500

    update

--- nativeplaces/src/components/PlaceList/PlaceList.js
+++ nativeplaces/src/components/PlaceList/PlaceList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { StyleSheet, ScrollView } from 'react-native';
 
 import ListItem from '../ListItem/ListItem';
 
@@ -8,12 +8,12 @@ const placeList = props => {
     <ListItem
       key={index}
       placeName={place}
-      onItemPressed={() => alert('Item pressed - ID: ' + index)}></ListItem>
+      onItemPressed={() => props.onItemDeleted(index)}></ListItem>
   ));
   return (
-    <View style={styles.listContainer}>
+    <ScrollView style={styles.listContainer}>
       {placesOutput}
-    </View>
+    </ScrollView>
   );
 };
 
