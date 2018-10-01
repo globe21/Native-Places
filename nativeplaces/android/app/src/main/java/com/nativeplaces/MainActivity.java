19d2c4f4fc74c76adcca8e943aac32635c3b4880
Shahram Ghassemi <shahramg92@hotmail.com>
2018-09-30 19:21:20 -0500

    add navigation files

--- nativeplaces/android/app/src/main/java/com/nativeplaces/MainActivity.java
+++ nativeplaces/android/app/src/main/java/com/nativeplaces/MainActivity.java
@@ -1,15 +1,6 @@
 package com.nativeplaces;
 
-import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.controllers.SplashActivity;
 
-public class MainActivity extends ReactActivity {
-
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "nativeplaces";
-    }
+public class MainActivity extends SplashActivity {
 }
