b5a7910f4cbe23da38d5fc8c808e42677b70bb94
Shahram Ghassemi <shahramg92@hotmail.com>
2018-10-05 16:41:29 -0500

    navigation updates

--- nativeplaces/App.js
+++ nativeplaces/App.js
@@ -1,9 +1,29 @@
 import { Navigation } from 'react-native-navigation';
+import { Provider } from 'react-redux';
 
 import AuthScreen from './src/screens/Auth/Auth';
+import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
+import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
+import configureStore from './src/store/configureStore';
+
+const store = configureStore();
 
 //Register Screens
-Navigation.registerComponent("native-places.AuthScreen", () => AuthScreen);
+Navigation.registerComponent(
+  "native-places.AuthScreen",
+   () => AuthScreen,
+    store,
+    Provider);
+Navigation.registerComponent(
+  "native-places.SharePlaceScreen",
+   () => SharePlaceScreen,
+    store,
+    Provider);
+Navigation.registerComponent(
+  "native-places.FindPlaceScreen",
+   () => FindPlaceScreen,
+    store,
+    Provider);
 
 // Start a App
 Navigation.startSingleScreenApp({
