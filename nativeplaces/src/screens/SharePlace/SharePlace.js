c92d11bcbf7f7f2c6adeb184f1ac63b81c056dcc
Shahram Ghassemi <shahramg92@hotmail.com>
2018-10-04 19:01:02 -0500

    update

---
+++ nativeplaces/src/screens/SharePlace/SharePlace.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native-navigation';
+
+class SharePlaceScreen extends Component {
+  render () {
+    return (
+      <View>
+        <Text>On SharePlaceScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default SharePlaceScreen;
