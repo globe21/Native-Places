cc0416bfc706c3521b5e6f7538a22056a362a1e3
Shahram Ghassemi <shahramg92@hotmail.com>
2018-10-09 23:10:22 -0500

    style updates

--- nativeplaces/src/screens/Auth/Auth.js
+++ nativeplaces/src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
 
@@ -10,12 +10,34 @@ class AuthScreen extends Component {
 
   render () {
     return (
-      <View>
-        <Text>Auth Screen</Text>
-        <Button title="Login" onPress={this.loginHandler}/>
+      <View style={styles.container}>
+        <Text>Please Log In</Text>
+        <Button title="Switch To Login" />
+        <View style={styles.inputContainer}>
+        <TextInput placeholder="Your E-Mail Address" style={styles.input}/>
+        <TextInput placeholder="Password"
+        style={styles.input}/>
+        <TextInput placeholder="Confirm Password"
+        style={styles.input}/>
+        </View>
+        <Button title="Submit" onPress={this.loginHandler}/>
       </View>
     );
   }
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  inputContainer: {
+    width: "80%"
+  },
+  input: {
+    width: "100%"
+  }
+})
 
 export default AuthScreen;
