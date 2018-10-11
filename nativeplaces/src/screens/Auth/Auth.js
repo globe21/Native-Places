0616dce55b77a1ccc8f44e7bea9c2e1104f7a8db
Shahram Ghassemi <shahramg92@hotmail.com>
2018-10-10 20:08:44 -0500

    setting global styling

--- nativeplaces/src/screens/Auth/Auth.js
+++ nativeplaces/src/screens/Auth/Auth.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
-import DefaultInput from '../../components/UI/DefaultInput';
+import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
+import HeadingText from '../../components/UI/HeadingText/HeadingText';
 
 class AuthScreen extends Component {
   loginHandler = () => {
@@ -12,12 +13,15 @@ class AuthScreen extends Component {
   render () {
     return (
       <View style={styles.container}>
-        <Text>Please Log In</Text>
+        <HeadingText>Please Log In</HeadingText>
         <Button title="Switch To Login" />
         <View style={styles.inputContainer}>
-        <DefaultInput placeholder="Your E-Mail Address" />
-        <DefaultInput placeholder="Password" />
-        <DefaultInput placeholder="Confirm Password" />
+        <DefaultInput placeholder="Your E-Mail Address"
+          style={styles.input}/>
+        <DefaultInput placeholder="Password"
+          style={styles.input}/>
+        <DefaultInput placeholder="Confirm Password"
+          style={styles.input}/>
         </View>
         <Button title="Submit" onPress={this.loginHandler}/>
       </View>
@@ -33,7 +37,11 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     width: "80%"
+  },
+  input: {
+    backgroundColor: "#eee",
+    borderColor: "#bbb"
   }
-})
+});
 
 export default AuthScreen;
