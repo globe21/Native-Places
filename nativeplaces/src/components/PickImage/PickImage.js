afc6b1e3c9e90e437b20274dca5b9399353a908b
Shahram Ghassemi <shahramg92@hotmail.com>
2018-10-11 15:11:29 -0500

    update SharePlace component

--- nativeplaces/src/components/PickImage/PickImage.js
+++ nativeplaces/src/components/PickImage/PickImage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View, Image, Button, StyleSheet } from 'react-native';
 
+import imagePlaceHolder from '../../assets/beautiful-place.jpg';
+
 class PickImage extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <View style={styles.placeholder}>
           <Image source={imagePlaceHolder} style={styles.previewImage} />
         </View>
@@ -17,6 +19,10 @@ class PickImage extends Component {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    alignItems: "center"
+  },
   placeholder: {
     borderWidth: 1,
     borderColor: "black",
