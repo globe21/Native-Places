f31951e261bc6dc02056b184bc23c24b056cc63b
Shahram Ghassemi <shahramg92@hotmail.com>
2018-10-19 19:03:14 -0500

    finish storing image on firebase with firebase cloud function

--- nativeplaces/functions/index.js
+++ nativeplaces/functions/index.js
@@ -14,34 +14,43 @@ const gcs = require('@google-cloud/storage')(gcconfig);
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.storeImage = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
+  return cors(request, response, () => {
     const body = JSON.parse(request.body);
     fs.writeFileSync("/tmp/uploaded-image.jpg", body.image, "base64", err => {
       console.log(err);
-      return response.status(500).json({error: err});
+      return response.status(500).json({ error: err });
     });
     const bucket = gcs.bucket('native-places-1539800355929.appspot.com');
     const uuid = UUID();
 
-    bucket.upload('/tmp/uploaded-image.jpg', {
-      uploadType: 'media',
-      destination: '/places/' + uuid + '.jpg',
-      metadata: {
-        contentType: 'image/jpeg',
-        firebaseStorageDownloadTokens: uuid
+    return bucket.upload(
+      "/tmp/uploaded-image.jpg",
+      {
+        uploadType: "media",
+        destination: "/places/" + uuid + ".jpg",
+        metadata: {
+          metadata: {
+            contentType: "image/jpeg",
+            firebaseStorageDownloadTokens: uuid
+          }
+        }
+      },
+      (err, file) => {
+        if (!err) {
+          return response.status(201).json({
+            imageUrl:
+              "https://firebasestorage.googleapis.com/v0/b/" +
+              bucket.name +
+              "/o/" +
+              encodeURIComponent(file.name) +
+              "?alt=media&token=" +
+              uuid
+          });
+        } else {
+          console.log(err);
+          return response.status(500).json({ error: err });
+        }
       }
-    }, (err, file) => {
-      if (!err) {
-        response.status(201).json({
-          imageUrl: "https://firebasestorage.googleapis.com/v0/b/" + bucket.name + '/o/' + encodeURIComponent(file.name) +
-          '?alt=media&token=' + uuid
-        });
-      } else {
-        console.log(err);
-        response.status(500).json({error: err})
-      }
-    });
+    );
   });
-
-  response.send("Hello from Firebase!");
 });
