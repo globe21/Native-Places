5f42e0c34f9923cd5e3736368d798caf67405c09
Shahram Ghassemi <shahramg92@hotmail.com>
2018-10-25 23:43:18 -0500

    finish all edits for app

--- nativeplaces/android/app/src/main/java/com/nativeplaces/MainActivity.java
+++ nativeplaces/android/app/src/main/java/com/nativeplaces/MainActivity.java
@@ -9,20 +9,20 @@ import android.util.TypedValue;
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
-  @Override
-  public LinearLayout createSplashLayout() {
-    LinearLayout view = new LinearLayout(this);
-    TextView textView = new TextView(this);
-
-    view.setBackgroundColor(Color.parseColor("#521751"));
-    view.setGravity(Gravity.CENTER);
-
-    textView.setTextColor(Color.parseColor("fa923f"));
-    textView.setText("Native Places - By Shahram Ghassemi");
-    textView.setGravity(Gravity.CENTER);
-    textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 40);
-
-    view.addView(textView);
-    return view;
-  }
+  // @Override
+  // public LinearLayout createSplashLayout() {
+  //   LinearLayout view = new LinearLayout(this);
+  //   TextView textView = new TextView(this);
+  //
+  //   view.setBackgroundColor(Color.parseColor("#521751"));
+  //   view.setGravity(Gravity.CENTER);
+  //
+  //   textView.setTextColor(Color.parseColor("fa923f"));
+  //   textView.setText("Native Places - By Shahram Ghassemi");
+  //   textView.setGravity(Gravity.CENTER);
+  //   textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 40);
+  //
+  //   view.addView(textView);
+  //   return view;
+  // }
 }
